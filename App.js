52f8c1a302b62584af9dd50b6d6a6e46704b9862
RaphaelQSmith <49982690+RaphaelQSmith@users.noreply.github.com>
2021-01-18 21:17:14 -0300

    Create 'Consultar' button

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>React Native is AWESOME</Text>
+      <Text>Consulte a Cotação das principais Criptomoedas</Text>
+      <Button 
+      title="Consultar" />
       <StatusBar style="auto" />
     </View>
   );
