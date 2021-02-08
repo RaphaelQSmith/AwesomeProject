cb9af23fe3c176897720acbd0a8d7fdfb27b8ef3
RaphaelQSmith <49982690+RaphaelQSmith@users.noreply.github.com>
2021-02-07 22:48:40 -0300

    Styling

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Consulte a Cotação das principais Criptomoedas</Text>
+      <Text style={styles.title}>Consulte a Cotação das principais Criptomoedas</Text>
       <Button 
-      title="Consultar" style={styles.myBt}/>
-      <Text>Cotação do Bitcoin hoje</Text>
-      <Text>Cotação do Ethereum hoje</Text>
-      <Text>Cotação do Dogecoin hoje</Text>
+      title="Consultar" style={styles.myBt} />
+      <Text style={styles.title}>Cotação do Bitcoin hoje</Text>
+      <Text style={styles.title}>Cotação do Ethereum hoje</Text>
+      <Text style={styles.title}>Cotação do Dogecoin hoje</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,13 +20,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#dca',
+    backgroundColor: '#222222',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  myBt: {
-    flex: 1, 
-    color: 'black'
-
-  }
+  title: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16,
+    padding: 15
+  },
+ 
 });
