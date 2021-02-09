e9cbc7157d1320a5a552bd6d42d997bbd19fec01
RaphaelQSmith <49982690+RaphaelQSmith@users.noreply.github.com>
2021-02-08 21:57:00 -0300

    Learning styling with CSS-like syntax

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableHighlightComponent, View } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Consulte a Cotação das principais Criptomoedas</Text>
+      <Text style={styles.title}>Cotação do Bitcoin hoje</Text>
+      <Button 
+      title="Consultar" style={styles.title} />
+      <Text style={styles.title}>Cotação do Ethereum hoje</Text>
       <Button 
       title="Consultar" style={styles.myBt} />
-      <Text style={styles.title}>Cotação do Bitcoin hoje</Text>
-      <Text style={styles.title}>Cotação do Ethereum hoje</Text>
       <Text style={styles.title}>Cotação do Dogecoin hoje</Text>
+      <Button 
+      style={styles.myBt}
+      title="Consultar" />
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -22,13 +28,16 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#222222',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+    marginTop: 22
   },
   title: {
     color: 'white',
     fontWeight: 'bold',
-    fontSize: 16,
-    padding: 15
+    fontSize: 16
   },
+  myBt:{
+
+  }
  
 });
